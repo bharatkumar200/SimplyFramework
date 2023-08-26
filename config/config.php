6ed089cb78439b01855e8a904bf3ced7166ccd06
Naman Kumar <namankumar80510@gmail.com>
2023-08-26 12:33:12 +0530

    Admin module added

--- config/config.php
+++ config/config.php
@@ -14,6 +14,7 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
+    \Admin\ConfigProvider::class,
     \Auth\ConfigProvider::class,
     \Core\ConfigProvider::class,
     \Mezzio\Tooling\ConfigProvider::class,
